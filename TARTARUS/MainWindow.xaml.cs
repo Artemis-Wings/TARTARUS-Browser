d1ed4bd8cd3aafaf4b8db273462a54dbd7d43b5a
Link4real <simon@die-duerrs.de>
2021-10-05 23:09:51 +0200

    yay

--- TARTARUS/MainWindow.xaml.cs
+++ TARTARUS/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CefSharp;
 using CefSharp.Wpf;
 
 namespace TARTARUS
@@ -20,115 +21,44 @@ namespace TARTARUS
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-        List<string> WebPages; // list of webpages visited since the browser was opened
-        int Current = 0; // to keep track of the current index of the webpages
+    {     
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void ChromiumWebBrowser_OnFrameLoadEnd(object sender, FrameLoadEndEventArgs e)
         {
-            WebPages = new List<string>();
-            GoHome();
-        }
-
-        void GoHome()
-        {
-            area.Text = "www.google.com";
-            chrome.Address = "www.google.com";
-            WebPages.Add("www.google.com");
-        }
-
-        void LoadWebPages(string Link, bool addToList = true)
-        {
-            area.Text = Link;
-            chrome.Address = Link;
-
-            MenuItem item = new MenuItem();
-            item.Click += MenuClicked;
-            item.Header = Link;
-            item.Width = 184;
-
-            Menu.Items.Add(item);
-
-            if (addToList)
+            Dispatcher.BeginInvoke((Action)(() => 
             {
-                Current++;
-                WebPages.Add(Link);
+                AddressBox.Text = e.Url;
+                BackBtn.IsEnabled = Browser.CanGoBack;
+                Navigate.IsEnabled = !string.IsNullOrWhiteSpace(AddressBox.Text);
+                ForwartBtn.IsEnabled = Browser.CanGoForward;
+            }));
+        }
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Browser.CanGoBack)
+            {
+                Browser.Back();
             }
         }
-
-        void ToggleWebPages(string Option)
+        private void ForwardBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (Option == "→")
+            if (Browser.CanGoForward)
             {
-                if ((WebPages.Count - Current - 1) != 0)
-                {
-                    Current++;
-                    LoadWebPages(WebPages[Current], false);
-                }
+                Browser.Forward();
             }
 
-            else
+
+
+        }
+        private void Navigate_Click(object sender, RoutedEventArgs e) 
+        { 
+            if(!string.IsNullOrWhiteSpace(AddressBox.Text))
             {
-                if ((WebPages.Count + Current - 1) >= WebPages.Count)
-                {
-                    Current--;
-                    LoadWebPages(WebPages[Current], false);
-                }
+                Browser.Address = AddressBox.Text;
             }
-        }
-
-        //back forw btns
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Button btn = (Button)sender;
-            ToggleWebPages(btn.Content.ToString());
-        }
-
-        //refresh
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            LoadWebPages(WebPages[Current]);
-        }
-
-        //home
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            LoadWebPages(WebPages[0]);
-        }
-
-        private void MenuClicked(object sender, RoutedEventArgs e)
-        {
-            MenuItem item = (MenuItem)sender;
-            LoadWebPages(item.Header.ToString());
-        }
-
-        private void area_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                LoadWebPages(area.Text);
-            }
-        }
-
-        //history
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-            if (WebPages.Count != 0)
-            {
-                Menu.PlacementTarget = hBTN;
-                Menu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
-                Menu.HorizontalOffset = -155;
-                Menu.IsOpen = true;
-            }
-        }
-
-        private void Button_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            e.Handled = true; //disable an event
         }
     }
 }
